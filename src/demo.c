450cf3b44778395d7e2c6df5c8c99777a72a32b4
Jakob Bowyer <jkbbwr@gmail.com>
2018-11-25 20:51:50 +0000

    Bug hunting

--- src/demo.c
+++ src/demo.c
@@ -7,21 +7,31 @@
 #include <string.h>
 #include <stdio.h>
 #include <vccrypt/compare.h>
+#include <vccrypt/key_agreement.h>
+#include <vccrypt/block_cipher.h>
+#include <vccrypt/stream_cipher.h>
+#include <vccert/error_codes.h>
 
-vccrypt_buffer_t *match_fragment(vccrypt_buffer_t *uuid, uint8_t *value, size_t size)
+EMSCRIPTEN_KEEPALIVE
+vccrypt_buffer_t *match_fragment(vccrypt_buffer_t *uuid, const uint8_t *value, size_t size)
 {
+    int result;
     vccert_parser_context_t fragment_parser;
     result = vccert_parser_init(&parser_opts, &fragment_parser, value, size);
-    if (result != 0)
+    if (result != VCCERT_STATUS_SUCCESS)
     {
         JS_THROW("vccert_parser_init: fragment");
     }
 
     const uint8_t *uuid_value;
     size_t uuid_size;
-    vccert_parser_find_short(
-        &parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_ENTITY_UUID, &uuid_value, &uuid_size
+    result = vccert_parser_find_short(
+        &fragment_parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_ENTITY_UUID, &uuid_value, &uuid_size
     );
+    if (result != VCCERT_STATUS_SUCCESS)
+    {
+        JS_THROW("vccert_parser_find_short");
+    }
 
     int match = crypto_memcmp(uuid->data, uuid_value, 16);
     if (0 != match)
@@ -32,18 +42,18 @@ vccrypt_buffer_t *match_fragment(vccrypt_buffer_t *uuid, uint8_t *value, size_t 
     const uint8_t *secret_value;
     size_t secret_size;
     vccert_parser_find_short(
-        &parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_FRAGMENT, &secret_value, &secret_size
+        &fragment_parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_FRAGMENT, &secret_value, &secret_size
     );
 
-    vccrypt_buffer_t buf = malloc(sizeof(vccrypt_buffer_t));
+    vccrypt_buffer_t *buf = malloc(sizeof(vccrypt_buffer_t));
     vccrypt_buffer_init(buf, &alloc_opts, secret_size);
-    memcpy(buf.data, secret_value, secret_size);
+    memcpy(buf->data, secret_value, secret_size);
 
     return buf;
 }
 
 EMSCRIPTEN_KEEPALIVE
-vccrypt_buffer_t *vwblockchain_find_shared_secret(vccrypt_buffer_t *cert, vccrypt_buffer_t *uuid)
+vccrypt_buffer_t *vwblockchain_get_field(vccrypt_buffer_t *cert, uint8_t field)
 {
     int result;
     vccert_parser_context_t parser;
@@ -55,10 +65,129 @@ vccrypt_buffer_t *vwblockchain_find_shared_secret(vccrypt_buffer_t *cert, vccryp
 
     const uint8_t *value;
     size_t size;
-    vccert_parser_find_short(&parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_FRAGMENT, &value, &size);
+    result = vccert_parser_find_short(&parser, field, &value, &size);
+    if (result != 0)
+    {
+        JS_THROW("vccert_parser_find_short");
+    }
 
-    vccrypt_buffer_t *buf;
-    buf = match_fragment(uuid, value, size);
+    vccrypt_buffer_t *buf = malloc(sizeof(vccrypt_buffer_t));
+    vccrypt_buffer_init(buf, &alloc_opts, size);
+    memcpy(buf->data, value, size);
+    return buf;
+}
+
+EMSCRIPTEN_KEEPALIVE
+vccrypt_buffer_t *vwblockchain_decrypt_field(vccrypt_buffer_t *secret, vccrypt_buffer_t *field)
+{
+    int result;
+
+    vccrypt_stream_options_t stream_options;
+    vccrypt_stream_options_init(&stream_options, &alloc_opts, VCCRYPT_STREAM_ALGORITHM_AES_256_2X_CTR);
+
+    vccrypt_stream_context_t stream;
+    vccrypt_stream_init(&stream_options, &stream, secret);
+
+    const uint8_t *in = (const uint8_t *) field->data;
+    size_t offset = 0;
+    size_t input_offset = 0;
+
+    vccrypt_buffer_t *buf = malloc(sizeof(vccrypt_buffer_t));
+    vccrypt_buffer_init(buf, &alloc_opts, field->size - stream.options->IV_size);
+
+    uint8_t *out = (uint8_t *) buf->data;
+
+    result = vccrypt_stream_start_decryption(&stream, in, &input_offset);
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_stream_start_decryption");
+    }
+
+    result = vccrypt_stream_decrypt(&stream, in + input_offset, field->size - input_offset, out, &offset);
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_stream_decrypt");
+    }
+
+    return buf;
+}
+
+EMSCRIPTEN_KEEPALIVE
+vccrypt_buffer_t *vwblockchain_decrypt_fragment(
+    vccrypt_buffer_t *public_key, vccrypt_buffer_t *private_key, vccrypt_buffer_t *fragment)
+{
+    int result;
+    vccrypt_buffer_t key_buffer;
+    vccrypt_block_context_t block;
+    vccrypt_block_options_t block_opts;
+    vccrypt_key_agreement_context_t context;
+
+    vccrypt_block_options_init(&block_opts, &alloc_opts, VCCRYPT_BLOCK_ALGORITHM_AES_256_2X_CBC);
+
+    vccrypt_suite_buffer_init_for_cipher_key_agreement_shared_secret(&crypto_suite, &key_buffer);
+    vccrypt_suite_cipher_key_agreement_init(&crypto_suite, &context);
+
+    result = vccrypt_key_agreement_long_term_secret_create(
+        &context, private_key, public_key, &key_buffer
+    );
+
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_key_agreement_long_term_secret_create");
+    }
+
+    result = vccrypt_block_init(&block_opts, &block, &key_buffer, false);
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_block_init");
+    }
+
+    if (fragment->size != 48)
+    {
+        JS_THROW("Fragment too small.");
+    }
+
+    const uint8_t *key_data = (const uint8_t *) fragment->data;
+
+
+    vccrypt_buffer_t *buf = malloc(sizeof(vccrypt_buffer_t));
+    vccrypt_buffer_init(buf, &alloc_opts, 32);
+
+    uint8_t *out = (uint8_t *) buf->data;
+
+    result = vccrypt_block_decrypt(&block, key_data, key_data + 16, out);
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_block_decrypt: first");
+    }
+    result = vccrypt_block_decrypt(&block, key_data + 16, key_data + 32, out + 16);
+    if (result != 0)
+    {
+        JS_THROW("vccrypt_block_decrypt: second");
+    }
+    return buf;
+}
+
+EMSCRIPTEN_KEEPALIVE
+vccrypt_buffer_t *vwblockchain_find_shared_secret(vccrypt_buffer_t *cert, vccrypt_buffer_t *uuid)
+{
+    int result;
+    vccert_parser_context_t parser;
+    result = vccert_parser_init(&parser_opts, &parser, cert->data, cert->size);
+    if (result != VCCERT_STATUS_SUCCESS)
+    {
+        JS_THROW("vccert_parser_init");
+    }
+
+    const uint8_t *value;
+    size_t size;
+    result = vccert_parser_find_short(&parser, VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_FRAGMENT, &value, &size);
+    if (result != VCCERT_STATUS_SUCCESS)
+    {
+        JS_THROW("vccert_parser_find_short");
+    }
+
+    vccrypt_buffer_t *buf = match_fragment(uuid, value, size);
     if (buf != NULL)
     {
         return buf;
@@ -66,9 +195,8 @@ vccrypt_buffer_t *vwblockchain_find_shared_secret(vccrypt_buffer_t *cert, vccryp
 
     do
     {
-        result = vccert_parser_find_short(
+        result = vccert_parser_find_next(
             &parser,
-            VCCERT_FIELD_TYPE_VELO_ENCRYPTED_SHARED_SECRET_FRAGMENT,
             &value,
             &size
         );
